cc8e068f1f261394eaf15715e408954429522fec
josephptflanagan <josephptflanagan@gmail.com>
2020-07-19 16:04:29 -0500

    updated app

--- app.js
+++ app.js
@@ -7,22 +7,102 @@ const generateReadme = require('./src/readme-template.js');
 //allows for the use of the inquirer package
 const inquirer = require("inquirer");
 
+//Prompts User for the project title, description, installation instructions, usage information, contribution guidelines, test instructions, license, GitHub Username, email address
 const promptUser = () => {
   return inquirer.prompt([
       {
+        //PROJECT TITLE INPUT
         type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
-        validate: nameInput => {
-        if (nameInput){
+        name: 'title',
+        message: 'What is the project title? (Required)',
+        validate: projectTitleInput => {
+        if (projectTitleInput){
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log('Please enter your project title!');
           return false;
         }
         }
       },
       {
+        //PROJECT DESCRIPTION INPUT
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: projectDescriptionInput => {
+        if (projectDescriptionInput){
+          return true;
+        } else {
+          console.log('Please enter your project description!');
+          return false;
+        }
+        }
+      },
+      {
+        //PROJECT INSTALLATION INSTRUCTIONS INPUT
+        type: 'input',
+        name: 'install',
+        message: 'Provide an installation instruction (Required)',
+        validate: projectInstallationInput => {
+        if (projectInstallationInput){
+          return true;
+        } else {
+          console.log('Please enter an installation instruction!');
+          return false;
+        }
+        }
+      },
+      {
+        //PROJECT USAGE INPUT
+        type: 'input',
+        name: 'usage',
+        message: 'Provide a description of proper usage (Required)',
+        validate: projectUsageInput => {
+        if (projectUsageInput){
+          return true;
+        } else {
+          console.log('Please enter information on usage!');
+          return false;
+        }
+        }
+      },
+      {
+        //PROJECT CONTRIBUTION GUIDELINES INPUT
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide a contribution guideline (Required)',
+        validate: projectContributionInput => {
+        if (projectContributionInput){
+          return true;
+        } else {
+          console.log('Please enter contribution guidelines!');
+          return false;
+        }
+        }
+      },
+      {
+        //PROJECT TEST INSTRUCTION INPUT
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions (Required)',
+        validate: projectTestInput => {
+        if (projectTestInput){
+          return true;
+        } else {
+          console.log('Please enter test instructions!');
+          return false;
+        }
+        }
+      },
+      {
+        //PROJECT LICENSE INPUT
+        type: 'list',
+        name: 'license',
+        message: 'What license is this covered by? (Choose the one that applies)',
+        choices: ['Apache', 'BSD 3-Clause', 'BSD 2-Clause', 'GPL', 'LGPL', 'MIT', 'Mozilla', 'Common Development and Dirstribution', 'Eclipse']
+      },
+      {
+        //GITHUB USERNAME INPUT
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub Username (Required)',
@@ -30,121 +110,40 @@ const promptUser = () => {
         if (gitHubUsernameInput){
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log('Please enter your username!');
           return false;
         }
         }
       },
       {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
-      },
-      {
+        //EMAIL ADDRESS INPUT
         type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => confirmAbout
+        name: 'email',
+        message: 'Enter your Email Address (Required)',
+        validate:emailInput => {
+        if (emailInput){
+          return true;
+        } else {
+          console.log('Please enter your email address!');
+          return false;
+        }
+        }
       }
-      
-    ]);
+    ])
+    .then(projectData => {
+        readmeData.projects.push(projectData);
+    });
   };
 
-const promptProject = portfolioData => {
-  // If there's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
-  console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
-  
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project?',
-      validate: projectNameInput => {
-      if (projectNameInput){
-        return true;
-      } else {
-        console.log('Please enter your name!');
-        return false;
-      }
-      }
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of the project (Required)',
-      validate: projectDescriptionInput => {
-      if (projectDescriptionInput){
-        return true;
-      } else {
-        console.log('Please enter your name!');
-        return false;
-      }
-      }
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)',
-      validate: gitHubLinkInput => {
-      if (gitHubLinkInput){
-        return true;
-      } else {
-        console.log('Please enter your name!');
-        return false;
-      }
-      }
-    },
-    {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ])
-  .then(projectData => {
-    portfolioData.projects.push(projectData);
-    if (projectData.confirmAddProject) {
-      return promptProject(portfolioData);
-    } else {
-      return portfolioData;
-    }
-  });
-};
-
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    return generateReadme(portfolioData);
+  .then(readmeData => {
+    return generateReadme(readmeData);
   })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
+  .then(pageMD => {
+    return writeFile(pageMD);
   })
   .then(writeFileResponse => {
     console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
   })
   .catch(err => {
     console.log(err);
